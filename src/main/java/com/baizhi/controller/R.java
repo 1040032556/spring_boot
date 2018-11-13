3234c7660b9a9ab6689f9541fa64bfb6f3ce3c41
amaryllis <15110530073@163.com>
2018-11-13 21:32:37 +0800

    yst second update

--- src/main/java/com/baizhi/controller/R.java
+++ src/main/java/com/baizhi/controller/R.java
@@ -3,5 +3,7 @@ package com.baizhi.controller;
 public class R {
     public static void main(String[] args) {
         System.out.println("00000000000000000000000000000000000");
+        System.out.println("0000000000000000000000000 0000000000");
+        System.out.println("0000000000000000000000000 0000000000");
     }
 }
